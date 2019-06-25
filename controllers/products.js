0f4e75dae36668ade4eb93ae047db3209de5b272
Paulo Pompeu <paulopompeu@adsl-172-10-3-136.dsl.sndg02.sbcglobal.net>
2019-06-24 23:34:17 -0500

    mvc adjustment

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,8 @@
-const products = [];
+const Product = require('../models/product')
+// const products = [];
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: "Add Product---",
         path: '/admin/add-product',
         formCSS: true,
@@ -11,18 +12,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    // products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
 };
