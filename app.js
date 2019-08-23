a0009a737e28f15d5d379487f95579a89aab7ea9
Paulo Pompeu <paulopompeu@adsl-172-10-3-136.dsl.sndg02.sbcglobal.net>
2019-08-23 16:34:30 -0500

    mongoose

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const path = require('path');
+const path = require("path");
+const { username, password } = require("./credentials/mongodb-atlas.json");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user')
+const errorController = require("./controllers/error");
+// const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5d23f2962d70477ce2021d01')
+  User.findById("5d23f2962d70477ce2021d01")
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
@@ -27,11 +28,19 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-})
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster0-euas3.mongodb.net/shop?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
