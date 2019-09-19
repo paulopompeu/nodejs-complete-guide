7f07fe4a92f699c567e7f798c71dbf1547efaef8
Paulo Pompeu <paulopompeu@adsl-172-10-3-195.dsl.sndg02.sbcglobal.net>
2019-09-19 15:07:40 -0500

    session implementation

--- app.js
+++ app.js
@@ -4,24 +4,41 @@ const { username, password } = require("./credentials/mongodb-atlas.json");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0-euas3.mongodb.net/shop?retryWrites=true&w=majority`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions"
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("5d644989e6fb04fcecdd770d")
-    .then(user => { 
+    .then(user => {
       req.user = user;
       // console.log('achou usuário' + req.user)
       next();
@@ -31,27 +48,25 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    `mongodb+srv://${username}:${password}@cluster0-euas3.mongodb.net/shop?retryWrites=true&w=majority`,
-    { useNewUrlParser: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Pompeu',
-          email: 'pompeu@test.com',
+          name: "Pompeu",
+          email: "pompeu@test.com",
           cart: {
             items: []
           }
         });
         user.save();
       }
-    })
+    });
     app.listen(3000);
   })
   .catch(err => {
---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+exports.getLogin = (req, res, next) => {
+  //   const isLoggedIn = req
+  //     .get("Cookie")
+  //     .split(";")[2]
+  //     .trim()
+  //     .split("=")[1];
+  // console.log(isLoggedIn)
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  req.session.isLoggedIn = true;
+  res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
